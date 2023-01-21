4c22a116e411e9ce1eefbac189b395b23301073a
Andrea Avallone <avalloneandrea@gmail.com>
2023-01-21 17:26:19 +0100

    Edit styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import packageInfo from '../../package.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'impastami-app';
+
+  appVersion: string;
+
+  constructor() {
+    this.appVersion = packageInfo.version;
+  }
+
 }
